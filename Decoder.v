5255404b6a1753fa20f2aa6fadaf3e24a391a319
ouharetaso <orangepi.hazimekara@gmail.com>
2024-10-03 23:32:48 +0900

    change decoder all reg to wire

--- Decoder.v
+++ Decoder.v
@@ -4,18 +4,18 @@
 module Decoder (
     input   wire    [31:0]  i_opcode,
     input   wire    [31:0]  i_pc,
-    output  reg     [4:0]   o_rd,
-    output  reg     [4:0]   o_rs1,
-    output  reg     [4:0]   o_rs2,
-    output  reg             o_en_imm,
-    output  reg     [31:0]  o_imm,
-    output  reg             o_en_jump,
-    output  reg     [31:0]  o_jump_addr,
-    output  reg     [3:0]   o_alu_op,
-    output  reg     [1:0]   o_jump,
-    output  reg             o_load,
-    output  reg             o_store,
-    output  reg             o_illegal_instruction
+    output  wire    [4:0]   o_rd,
+    output  wire    [4:0]   o_rs1,
+    output  wire    [4:0]   o_rs2,
+    output  wire            o_en_imm,
+    output  wire    [31:0]  o_imm,
+    output  wire            o_en_jump,
+    output  wire    [31:0]  o_jump_addr,
+    output  wire    [3:0]   o_alu_op,
+    output  wire    [1:0]   o_jump,
+    output  wire            o_load,
+    output  wire            o_store,
+    output  wire            o_illegal_instruction
 );
     wire    [6:0]   opcode;
     wire    [4:0]   rd;
@@ -29,8 +29,7 @@ module Decoder (
     wire    [31:0]  B_immediate;
     wire    [31:0]  S_immediate;
     
-    
-
+    /*
     initial begin
         o_rd                    = 0;
         o_rs1                   = 0;
@@ -45,6 +44,7 @@ module Decoder (
         o_store                 = 0;
         o_illegal_instruction   = 1;
     end
+    */
 
     assign opcode       = i_opcode[6:0];
     assign rd           = i_opcode[11:7];
@@ -72,6 +72,46 @@ module Decoder (
     parameter opcode_LOAD       = 7'b0000011;
     parameter opcode_STORE      = 7'b0100011;
 
+    assign o_rd                 = (opcode == opcode_OP      || opcode == opcode_OP_IMM  || opcode == opcode_LUI || opcode == opcode_JAL ||
+                                   opcode == opcode_JALR    || opcode == opcode_AUIPC   || opcode == opcode_JAL) ? rd : 0;
+
+    assign o_rs1                = (opcode == opcode_OP      || opcode == opcode_OP_IMM  || opcode == opcode_JALR || opcode == opcode_BRANCH ||
+                                   opcode == opcode_LOAD    || opcode == opcode_STORE) ? rs1 : 0;
+
+    assign o_rs2                = (opcode == opcode_OP      || opcode == opcode_BRANCH  || opcode == opcode_STORE) ? rs2 : 0;
+
+    assign o_en_imm             = (opcode == opcode_OP_IMM  || opcode == opcode_AUIPC   || opcode == opcode_LUI || opcode == opcode_JAL ||
+                                   opcode == opcode_JALR    || opcode == opcode_LOAD    || opcode == opcode_STORE) ? 1 : 0;
+
+    assign o_imm                = (opcode == opcode_AUIPC   || opcode == opcode_LUI) ? U_immediate :
+                                  (opcode == opcode_JALR    || opcode == opcode_LOAD) ? I_immediate : 
+                                  (opcode == opcode_OP_IMM) ? ((funct3 == 3'b001 || funct3 == 3'b101) ? {{27{1'b0}},rs2} : I_immediate) :
+                                  (opcode == opcode_JAL) ? i_pc + 4 : 
+                                  (opcode == opcode_STORE) ? S_immediate : 0;
+    
+    assign o_en_jump            = (opcode == opcode_AUIPC   || opcode == opcode_BRANCH  || opcode == opcode_JAL || opcode == opcode_JALR)
+                                ? 1 : 0;
+    
+    assign o_jump_addr          = (opcode == opcode_AUIPC   || opcode == opcode_JAL || opcode == opcode_JALR) ? 
+                                  ((opcode == opcode_JAL) ? i_pc + J_immediate : i_pc) : i_pc + B_immediate;
+    
+    assign o_alu_op             = (opcode == opcode_OP      || opcode == opcode_OP_IMM) ? 
+                                  ((funct3 == 3'b001 || funct3 == 3'b101) ? {funct7[5],funct3} : funct3) : 
+                                  (opcode == opcode_BRANCH) ? {1'b1,-funct3} : 4'b0000;
+
+    assign o_jump               = (opcode == opcode_JAL) ? 1 : 
+                                  (opcode == opcode_JALR) ? 2 : 
+                                  (opcode == opcode_BRANCH) ? 3 : 0;
+    
+    assign o_load               = (opcode == opcode_LOAD) ? 1 : 0;
+
+    assign o_store              = (opcode == opcode_STORE) ? 1 : 0;
+    
+    assign o_illegal_instruction= (opcode == opcode_OP || opcode == opcode_OP_IMM || opcode == opcode_SYSTEM || opcode == opcode_AUIPC ||
+                                   opcode == opcode_LUI || opcode == opcode_JAL || opcode == opcode_JALR || opcode == opcode_BRANCH ||
+                                   opcode == opcode_LOAD || opcode == opcode_STORE) ? 0 : 1;
+    
+    /*
     always @(*) begin
         case(opcode)
             opcode_OP: begin
@@ -209,5 +249,6 @@ module Decoder (
             end
         endcase
     end
+    */
 
 endmodule
